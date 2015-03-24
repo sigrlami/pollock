9280412bf74ffc91bb3d8e29721ba345911a5441
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2015-03-25 01:01:26 +0200

    add snaplet mechanism

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,26 @@
 import Snap
 import qualified Data.Text as T
 
-site :: Snap () 
-site = writeText $ T.pack "Hello, Pollock!"
+-- | The Pollock type identifies our application 
+--   and holds anything our snaplet needs to function.
+data Pollock = Pollock { } 
+
+-- | The indexHandler will be invoked whenever someone 
+--   accesses the root URL, "/".
+indexHandler :: Handler Pollock Pollock ()
+indexHandler = writeText $ T.pack "Hello, world!"
+
+-- | Build a new Pollock snaplet.
+memoiseInit :: SnapletInit Pollock Pollock
+memoiseInit = 
+  makeSnaplet "pollock" 
+              "Best pollin system!" 
+              Nothing 
+  $ do
+      addRoutes [("", indexHandler)]
+      return $ Memoise { }
 
 main :: IO ()
-main = quickHttpServe site
+main = do
+  (_, site, _) <- runSnaplet Nothing memoiseInit -- Init Pollock snaplet
+  quickHttpServe site -- Start the Snap server

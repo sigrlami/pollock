1e20a194231134c30267677efa78a3036eeb794a
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2015-03-25 22:44:14 +0200

    add auth and template

---
+++ snaplets/heist/templates/signup.tpl
@@ -0,0 +1,12 @@
+<apply template="base">
+    <bind tag="pagetitle">Sign up</bind>
+    <form method="POST">
+        <fieldset>
+            <label for="username">Username</label><br />
+            <input type="text" name="username" id="username" /><br />
+            <label for="password">Password</label><br />
+            <input type="password" name="password" id="password" /><br />
+            <input type="submit" value="Submit" />
+        </fieldset>
+    </form>
+</apply>
--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,20 @@
+import Control.Applicative
+import Control.Lens
 import Snap
 import Snap.Snaplet.Heist
-import Control.Lens
+import Snap.Snaplet.Session
+import Snap.Snaplet.Auth
+import Snap.Snaplet.Auth.Backends.JsonFile
+import Snap.Snaplet.Session.Backends.CookieSession
 import Snap.Util.FileServe
+import Heist.SpliceAPI
+import qualified Heist.Interpreted as I
 import qualified Data.Text as T
 
--- | The Pollock type identifies our application 
---   and holds anything our snaplet needs to function.
 data Pollock 
   = Pollock { _heist :: Snaplet (Heist Pollock)
+            , _sess  :: Snaplet SessionManager
+            , _auth  :: Snaplet (AuthManager Pollock)  
             }
 
 makeLenses ''Pollock
@@ -17,22 +24,57 @@ instance HasHeist Pollock where
  
 -- | The indexHandler will be invoked whenever someone 
 --   accesses the root URL, "/".
-indexHandler :: Handler Pollock Pollock ()
-indexHandler = render "index"
+handlerIndex :: Handler Pollock Pollock ()
+handlerIndex = render "index"
+
+-- Used to output an error to the user where needed.
+renderError :: Show a => a -> Handler Pollock (AuthManager Pollock) ()
+renderError = renderError' . show
+renderError' :: String -> Handler Pollock (AuthManager  Pollock) ()
+renderError' s = renderWithSplices "_error" $ "errormsg" ## I.textSplice . T.pack $ s
+
+-- Triggers on the /signup page
+-- Form is shown on GET requests, form is handled on POST requests.
+-- This idea is used on every page with a form.
+handlerSignup :: Handler Pollock (AuthManager Pollock) ()
+handlerSignup = method GET handleForm <|> method POST handleFormSubmit
+    where
+        handleForm = render "signup"
+        handleFormSubmit = registerUser "username" "password" >>= afterRegister
+        afterRegister :: Either AuthFailure AuthUser -> Handler Pollock (AuthManager Pollock) ()
+        afterRegister (Left af) = renderError af
+        afterRegister _ = render "_signupsuccess"
+
+-- Triggers on the /signin page
+handlerLogin :: Handler Pollock (AuthManager Pollock) ()
+handlerLogin = method GET handleForm <|> method POST handleFormSubmit
+    where
+        handleForm = render "signin"
+        handleFormSubmit = loginUser "username" "password" Nothing renderError (redirect "/")
+
+-- Triggers on the /signout page
+handlerLogout :: Handler Pollock (AuthManager Pollock) ()
+handlerLogout = logout >> redirect "/"
 
 -- | Build a new Pollock snaplet.
-memoiseInit :: SnapletInit Pollock Pollock
-memoiseInit = 
+pollockInit :: SnapletInit Pollock Pollock
+pollockInit = 
   makeSnaplet "pollock" 
               "Best polling system!" 
               Nothing 
   $ do
       h <- nestSnaplet "heist" heist $ heistInit "templates"
-      addRoutes [ ("static", serveDirectory "static")
-                , ("", indexHandler)]
-      return $ Pollock { _heist = h }
+      s <- nestSnaplet "sess" sess $ initCookieSessionManager "site_key.txt" "sess" (Just 3600)
+      a <- nestSnaplet "auth" auth $ initJsonFileAuthManager defAuthSettings sess "users.json"
+      addRoutes [ ("static" , serveDirectory "static")
+                , (""       , handlerIndex)
+                , ("/signup", with auth handlerSignup)
+                , ("/login" , with auth handlerLogin)
+                , ("/logout", with auth handlerLogout) 
+                ]
+      return $ Pollock { _heist = h, _sess = s, _auth = a }
 
 main :: IO ()
 main = do
-  (_, site, _) <- runSnaplet Nothing memoiseInit
+  (_, site, _) <- runSnaplet Nothing pollockInit
   quickHttpServe site
